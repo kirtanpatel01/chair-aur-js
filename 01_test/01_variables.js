3813ec14ccd241dc2519ea4875930fb3fef7fcf7
Kirtan Patel <159251485+kirtanpatel01@users.noreply.github.com>
2025-06-18 05:00:00 +0000

    added variables

---
+++ 01_test/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+var accountName = "Kiton Patel"
+let accountValue = 10003.42
+accountCity = "Vadodara"
+let accountState;
+
+console.table([accountId, accountName, accountValue, accountCity, accountState ])
