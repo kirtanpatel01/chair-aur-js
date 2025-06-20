121b3fac3332241919615555cc82deced9f5696a
Kirtan Patel <159251485+kirtanpatel01@users.noreply.github.com>
2025-06-20 15:19:26 +0000

    arrays done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+const naruto = [ "naruto", "sasuke", "kakashi", "madara", "hashirama", "minato" ]
+const aot = [ "eren", "mikasa", "armen", "levy", "sanji", "annny" ]
+
+// naruto.push(aot)
+// let newarr = naruto.concat(aot)
+// let newarr = [...naruto, ...aot]
+
+const nums = [1,2,[3,4,5,[6,7],8,[9,[10], [11, [12, 13, 14, [15, 16, [17, 18, 19, [20]]]]]]]]
+
+// console.log(nums.flat(Infinity))
+
+console.log(Array.isArray(nums))
+console.log(Array.from("kiton"))
+console.log(Array.of("kiton", 12334))
